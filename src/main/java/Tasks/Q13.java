0d1d811673f5c02cf3abb88fa5276c3f98b38309
icarus-77 <136583278+icarus-77@users.noreply.github.com>
2023-07-31 11:32:49 +0400

    Base of the project

--- src/main/java/Tasks/Q13.java
+++
@@ -1,9 +0,0 @@
-package Tasks;
-
-public class Q13 {
-    //Find the countries with the minimum and the maximum population.
-    public static String minAndMaxCountriesByPopulation() {
-        //Overdoing Q11 resulted not doing anything here))
-        return Q11.countryPopulationValues();
-    }
-}
